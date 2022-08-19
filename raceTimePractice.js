542583de1a5570ac71e84576f3be28cc8bdccd92
KonaGoals <eduardovalentinoleal@gmail.com>
2022-08-19 02:48:07 -0500

    I love coding

--- raceTimePractice.js
+++ raceTimePractice.js
@@ -1,16 +1,18 @@
 let raceNumber = Math.floor(Math.random() * 1000);
 
 let early = true;
-let age = 17;
+let age = 18;
 
 if (age && early  > 18){
  raceNumber += 1000
 } 
-if (age >= 18 && early){
+if (age > 18 && early){
   console.log(`Racer ${raceNumber} will start at 9:30 am`)
 } else if (age > 18 && !early){
   console.log(`If runner ${raceNumber} is not over 18 AND did not register early they will race at 11:00am`);
 } 
 if (age < 18){
   console.log('Under 18 will start at 12:30 pm regardless of registration time')
+} else if (age === 18 && early){
+  console.log('getcha ass to da registration for further assitance')
 }
