c38e5142845953d5fb3b1c8391ef66aa4fcea0fc
KonaGoals <eduardovalentinoleal@gmail.com>
2022-09-03 12:36:36 -0500

    dog years quick practice project

---
+++ anotherPract.js
@@ -0,0 +1,14 @@
+const myAge = 28;
+
+let earlyYears = 2;
+
+earlyYears *= 10.5;
+
+let laterYears = myAge - 2;
+
+laterYears *= 4
+
+const myAgeInDogYears = laterYears + earlyYears;
+
+const myName = 'Eduardo Valentino Leal'.toUpperCase();
+console.log(`My name is ${myName} and I am ${myAge} years old. That's ${laterYears} in dog years!`)
