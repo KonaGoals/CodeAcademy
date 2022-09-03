1edf456f4f66bade68fff5566d16905ae845b703
KonaGoals <eduardovalentinoleal@gmail.com>
2022-09-02 23:00:04 -0500

    randomizer added

--- rockPaperScissors.js
+++ rockPaperScissors.js
@@ -6,3 +6,17 @@ const getUserChoice = userInput => {
       console.log('not a valid option');
     }
   }
+
+  function getComputerChoice() {
+    let getComputerChoice = Math.floor(Math.random() * 3);
+    switch(getComputerChoice){
+      case 0:
+      return 'rock';
+      case 1:
+      return 'paper';
+      case 2:
+      return 'scissors'
+    
+    }
+    }
+    console.log(getComputerChoice());
