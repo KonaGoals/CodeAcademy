ff80aee3cd983c6df870c764445ccf7ea8f3d08d
KonaGoals <eduardovalentinoleal@gmail.com>
2022-09-03 12:15:59 -0500

    user input for bomb was returning undefined due to a punctuation error. Fixed by making BOMB TO 'bomb'. Had no idea making it capitalized would cause so many issues so its good to know to keep assigned inputs lower case I guess

--- rockPaperScissors.js
+++ rockPaperScissors.js
@@ -1,91 +1,68 @@
-const getUserChoice = userInput => {
+const getUserChoice = (userInput) => {
     userInput = userInput.toLowerCase();
-    if (userInput === 'rock' || userInput === 'paper' || userInput === 'scissors'){
-      return userInput; 
-    } else { 
-      console.log('not a valid option');
+    if (
+      userInput === "rock" ||
+      userInput === "paper" ||
+      userInput === "scissors" ||
+      userInput === 'bomb'
+    ) {
+      return userInput;
+    } else {
+      console.log("not a valid option");
     }
-  }
-
+  };
+  
   function getComputerChoice() {
     let getComputerChoice = Math.floor(Math.random() * 3);
-    switch(getComputerChoice){
+    switch (getComputerChoice) {
       case 0:
-      return 'rock';
+        return "rock";
       case 1:
-      return 'paper';
+        return "paper";
       case 2:
-      return 'scissors'
-    
+        return "scissors";
     }
+  }
+  
+  function determineWinner(userChoice, computerChoice) {
+     if (userChoice === 'bomb'){
+      return 'Da bomb always wins'
     }
-    console.log(getComputerChoice());
-
-    const getUserChoice = (userInput) => {
-        userInput = userInput.toLowerCase();
-        if (
-          userInput === "rock" ||
-          userInput === "paper" ||
-          userInput === "scissors" ||
-          userInput === 'BOMB'
-        ) {
-          return userInput;
-        } else {
-          console.log("not a valid option");
-        }
-      };
-      
-      function getComputerChoice() {
-        let getComputerChoice = Math.floor(Math.random() * 3);
-        switch (getComputerChoice) {
-          case 0:
-            return "rock";
-          case 1:
-            return "paper";
-          case 2:
-            return "scissors";
-        }
+    if (userChoice === computerChoice) {
+      return "Tie game, play again";
+    }
+    if (userChoice === "rock") {
+      if (computerChoice === "paper") {
+        return "Computer wins bih";
+      } else {
+        return "User wins!";
       }
-      
-      function determineWinner(userChoice, computerChoice) {
-         if (userChoice === 'BOMB'){
-          return 'Da bomb always wins'
-        }
-        if (userChoice === computerChoice) {
-          return "Tie game, play again";
-        }
-        if (userChoice === "rock") {
-          if (computerChoice === "paper") {
-            return "Computer wins :/";
-          } else {
-            return "User wins!";
-          }
-        }
-      
-        if (userChoice === "paper") {
-          if (computerChoice === "scissors") {
-            return "computer Wins :/";
-          } else {
-            return "User wins!";
-          }
-        }
-        if (userChoice === "scissors") {
-          if (computerChoice === "rock") {
-            return "computer Wins :/";
-          } else {
-            return "YOU WIN MUHFUGGUH";
-          }
-        }
+    }
+  
+    if (userChoice === "paper") {
+      if (computerChoice === "scissors") {
+        return "computer Wins";
+      } else {
+        return "User wins!";
       }
-      
-      function playGame() {
-        const userChoice = getUserChoice("scissors", "rock", "paper");
-        const computerChoice = getComputerChoice();
-        console.log('You rolled ' + userChoice);
-        console.log('The CPU rolled ' + computerChoice);
-       console.log(determineWinner(userChoice, computerChoice));
-      
-      };
-       
-      playGame();
-      
+    }
+    if (userChoice === "scissors") {
+      if (computerChoice === "rock") {
+        return "computer Wins :/";
+      } else {
+        return "YOU WIN MUHFUGGUH";
+      }
+    }
+  }
+  
+  function playGame() {
+    const userChoice = getUserChoice('bomb');
+    const computerChoice = getComputerChoice();
+    console.log('You rolled ' + userChoice);
+    console.log('The CPU rolled ' + computerChoice);
+   console.log(determineWinner(userChoice, computerChoice));
+  
+  };
+   
+  playGame();
+  
