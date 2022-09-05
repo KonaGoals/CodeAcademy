acb5f4f5829e6fe3b6662c71c8cfb65cee2889fe
KonaGoals <eduardovalentinoleal@gmail.com>
2022-09-05 03:55:22 -0500

    cleaned up bugs and successfully completed project, issue was a set of curly braces that shouldn't have been implemented because the equation was implicitly returned thus rendering the use of return or curly braces to cause an error

--- sleepDebtCalc.js
+++ sleepDebtCalc.js
@@ -1,19 +1,19 @@
-const getSleepHours = day => {
+const getSleepHours = (day) => {
     switch (day) {
       case "monday":
-        return 4;
+        return 6;
         break;
       case "tuesday":
         return 10;
         break;
       case "wednesday":
-        return 3;
+        return 6;
         break;
       case "thursday":
         return 8;
         break;
       case "friday":
-        return 5;
+        return 8;
         break;
       case "saturday":
         return 6;
@@ -22,26 +22,51 @@ const getSleepHours = day => {
         return 6.5;
         break;
       default:
-       return "Why is this not working";
-    };
+        return "Why is this not working";
+    }
   };
   
-  const getActualSleepHours = () => {
-    getSleepHours('monday') + getSleepHours('day') + getSleepHours('tuesday') +  getSleepHours('wednesday') + getSleepHours('thursday') + getSleepHours('friday') + getSleepHours('saturday') + getSleepHours('sunday')
-  }
+  const getActualSleepHours = () => 
+    getSleepHours("monday") +
+      getSleepHours("tuesday") +
+      getSleepHours("wednesday") +
+      getSleepHours("thursday") +
+      getSleepHours("friday") +
+      getSleepHours("saturday") +
+      getSleepHours("sunday");
+  
   
   const getIdealSleepHours = () => {
-    let idealHours = 8
+    let idealHours = 7;
     return idealHours * 7;
-  }
+  };
   
   const calculateSleepDebt = () => {
-    let actualSleepHours = getActualSleepHours()
-    let idealSleepHours = getIdealSleepHours()
-  }
+    const actualSleepHours = getActualSleepHours();
+    const idealSleepHours = getIdealSleepHours();
   
-  const results = () => {
-    
-  }
+    if (actualSleepHours === idealSleepHours) {
+      console.log(
+        "Poifect! Keep it up and you'll see them gains sooner than later"
+      );
+    } else if (actualSleepHours > idealSleepHours) {
+      console.log(
+        "You got " +
+          (actualSleepHours - idealSleepHours) +
+          "more hour(s) than needed. You should use that time to study or exercise."
+      );
+    } else if (actualSleepHours < idealSleepHours) {
+      console.log(
+        "You got " +
+          (idealSleepHours - actualSleepHours) +
+          "less hour(s) less than you needed this week. Get some more Zs before you lose them gainzzzz"
+      );
+    } else {
+      console.log("Error! Something went wrong. Check your code.");
+    }
+  };
   
-  console.log(getSleepHours('wednesday'));
+  calculateSleepDebt();
+  
+
+// Language: javascript
